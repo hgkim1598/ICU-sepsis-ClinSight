64d9c9e2b65a62ebfa382d6b57f8f866fef287dd
beumjin <beumjin1209@gmail.com>
2026-04-15 17:16:44 +0900

    resolve merge conflict in loader.py

--- pipeline/mortality/loader.py
+++ pipeline/mortality/loader.py
@@ -4,18 +4,13 @@ import boto3
 import joblib
 import torch
 import xgboost as xgb
-import tempfile
-tmp = tempfile.gettempdir()
-
 from io import BytesIO
 
 from config import S3_BUCKET, MODEL_PREFIX, USE_S3, LOCAL_MODEL_PATH
 from model import BiLSTM
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-TMP_XGB = os.path.join(tempfile.gettempdir(), 'xgb_stacking.json')
-TMP_LR  = os.path.join(tempfile.gettempdir(), 'stacking_lr_oof.pkl')
+tmp = tempfile.gettempdir()
 
 _bilstm, _clf_xgb, _lr = None, None, None
 
@@ -27,7 +22,6 @@ def _load_models():
         obj = s3.get_object(Bucket=S3_BUCKET, Key=f'{MODEL_PREFIX}/mortality_bilstm.pt')
         state = torch.load(BytesIO(obj['Body'].read()), map_location=device)
 
-<<<<<<< HEAD
         obj = s3.get_object(Bucket=S3_BUCKET, Key=f'{MODEL_PREFIX}/mortality_xgb.json')
         with open(os.path.join(tmp, 'mortality_xgb.json'), 'wb') as f:
             f.write(obj['Body'].read())
@@ -37,38 +31,18 @@ def _load_models():
             f.write(obj['Body'].read())
     else:
         import shutil
-        state = torch.load(f'{LOCAL_MODEL_PATH}/mortality_bilstm.pt', map_location=device)
-        shutil.copy(f'{LOCAL_MODEL_PATH}/mortality_xgb.json',       '/tmp/mortality_xgb.json')
-        shutil.copy(f'{LOCAL_MODEL_PATH}/mortality_stacking_lr.pkl', '/tmp/mortality_stacking_lr.pkl')
-=======
-        obj = s3.get_object(Bucket=S3_BUCKET, Key=f'{MODEL_PREFIX}/xgb_stacking.json')
-        with open(TMP_XGB, 'wb') as f:
-            f.write(obj['Body'].read())
-
-        obj = s3.get_object(Bucket=S3_BUCKET, Key=f'{MODEL_PREFIX}/OOF/stacking_lr_oof.pkl')
-        with open(TMP_LR, 'wb') as f:
-            f.write(obj['Body'].read())
-    else:
-        import shutil
-        state = torch.load(f'{LOCAL_MODEL_PATH}/bilstm_best.pt', map_location=device)
-        shutil.copy(f'{LOCAL_MODEL_PATH}/xgb_stacking.json',       TMP_XGB)
-        shutil.copy(f'{LOCAL_MODEL_PATH}/OOF/stacking_lr_oof.pkl', TMP_LR)
->>>>>>> beumjin-test
+        state = torch.load(os.path.join(LOCAL_MODEL_PATH, 'mortality_bilstm.pt'), map_location=device)
+        shutil.copy(os.path.join(LOCAL_MODEL_PATH, 'mortality_xgb.json'),        os.path.join(tmp, 'mortality_xgb.json'))
+        shutil.copy(os.path.join(LOCAL_MODEL_PATH, 'mortality_stacking_lr.pkl'), os.path.join(tmp, 'mortality_stacking_lr.pkl'))
 
     bilstm = BiLSTM().to(device)
     bilstm.load_state_dict(state)
     bilstm.eval()
 
     clf_xgb = xgb.XGBClassifier()
-<<<<<<< HEAD
     clf_xgb.load_model(os.path.join(tmp, 'mortality_xgb.json'))
 
     lr = joblib.load(os.path.join(tmp, 'mortality_stacking_lr.pkl'))
-=======
-    clf_xgb.load_model(TMP_XGB)
-
-    lr = joblib.load(TMP_LR)
->>>>>>> beumjin-test
 
     return bilstm, clf_xgb, lr
 
